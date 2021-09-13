d3139e79f93d368521ecff002d29739e34495226
sashimi390 <67630672+sashimi390@users.noreply.github.com>
2021-09-13 15:16:29 +0900

    change tabColor

--- src/App.js
+++ src/App.js
@@ -63,8 +63,8 @@ const App = () => {
               <Route exact path="/setting/Guide" component={Guide} />
             </IonRouterOutlet>
 
-            <IonTabBar slot="bottom" color="dark">
-              <IonTabButton tab="home" href="/home">
+            <IonTabBar slot="bottom" color="fortabcol">
+              <IonTabButton className="TabButton" tab="home" href="/home">
                 <IonIcon icon={peopleOutline} />
                 <IonLabel>プロフィール</IonLabel>
               </IonTabButton>
